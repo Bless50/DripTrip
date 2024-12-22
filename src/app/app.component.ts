e4002ba62ed6495e005462f5ec8b338ff6dc1ee3
Bless50 <blessn691@.com>
2024-12-23 00:07:47 +0100

    feat(navbar): implement responsive navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [NavbarComponent],
+  template: `<app-navbar></app-navbar>`,
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public title = 'DripTrip';
-
-  constructor() {
-    this.title = 'DripTrip';
-  }
-
-  public ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class NavbarComponent {
+  public isMobileMenuOpen = false;
+
+  public toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+}
